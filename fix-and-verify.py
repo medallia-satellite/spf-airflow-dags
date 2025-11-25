eaa096fab3924738865aac7b8de3f7211f908736
Emmanuel <echang@medallia.com>
2025-11-25 11:31:04 -0300

    troubleshooting

--- fix-and-verify.py
+++ fix-and-verify.py
@@ -83,8 +83,8 @@ def es_poc_dag():
 
     grouped = group_by_base_alias(fetch_data.output)
     alias_group.partial().expand(
-        base_alias=grouped.map(lambda x: x["base_alias"]),
-        aliases=grouped.map(lambda x: x["aliases"]),
+        base_alias=[x["base_alias"] for x in grouped],
+        aliases=[x["aliases"] for x in grouped],
     )
 
 
