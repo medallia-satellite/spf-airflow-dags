07ca2c7d278120f81b3b0db0692511ca2fdeb202
Emmanuel <echang@medallia.com>
2025-11-26 16:12:49 -0300

    troubleshooting

--- fix-and-verify.py
+++ fix-and-verify.py
@@ -8,16 +8,16 @@ from airflow.decorators import task, dag, task_group
 from airflow.providers.http.hooks.http import HttpHook
 from dateutil.relativedelta import relativedelta
 
-base_pattern = r"(\w+)_topic-builder(-\w+)+(\.\w{2,4}){0,2}(\.\w+)(\.\w{2,4}){1,2}-\1"
-base_regex = re.compile(base_pattern)
+BASE_PATTERN = r"(\w+)_topic-builder(-\w+)+(\.\w{2,4}){0,2}(\.\w+)(\.\w{2,4}){1,2}-\1"
+BASE_REGEX = re.compile(BASE_PATTERN)
 
-regex_mapping = {
-    "read": re.compile(rf"^{base_pattern}$"),
-    "write": re.compile(rf"^{base_pattern}" + r"-[0-9]{4}-[0-9]{2}-[0-9]{2}$"),
-    "rollover": re.compile(rf"^{base_pattern}-rollover$"),
+REGEX_MAPPING = {
+    "read": re.compile(rf"^{BASE_PATTERN}$"),
+    "write": re.compile(rf"^{BASE_PATTERN}" + r"-[0-9]{4}-[0-9]{2}-[0-9]{2}$"),
+    "rollover": re.compile(rf"^{BASE_PATTERN}-rollover$"),
 }
 
-policy_mapping = {
+POLICY_MAPPING = {
 	"M6": 6,
 	"M6_rollover": 6,
 	"M18": 18,
@@ -39,60 +39,53 @@ def monthly_aliases(alias: str, start_date: datetime.date, num_months: int) -> I
     catchup=False,
 )
 def es_poc_dag():
-    hook = HttpHook(method='GET', http_conn_id='es-wordtags')
+    hook_get = HttpHook(method='GET', http_conn_id='es-wordtags')
 
-    @task()
+    @task
     def fetch_aliases():
-        response = hook.run(
+        response = hook_get.run(
             endpoint='/_cat/aliases?h=alias,index,is_write_index',
             headers={'Accept': 'application/json'},
         )
-        hook.check_response(response)
-        return [r for r in response.json() if base_regex.match(r["alias"])][:10]
+        hook_get.check_response(response)
+        return [r for r in response.json() if BASE_REGEX.match(r["alias"])][:10]
 
     @task
-    def fetch_policies(alias):
-        response = hook.run(
-            endpoint=f'/{alias}/_settings/index.lifecycle.name',
+    def fetch_alias_settings(alias):
+        response = hook_get.run(
+            endpoint=f'/{alias}/_settings/index.lifecycle.name,index.lifecycle.rollover_alias,index.analysis.filter.compound_capture.patterns',
             headers={'Accept': 'application/json'},
         )
-        hook.check_response(response)
-        return set(p["settings"]["index"]["lifecycle"]["name"] for _, p in response.json().items())
+        hook_get.check_response(response)
+        return response.json()
 
     @task
-    def fetch_mappings(alias):
-        response = hook.run(
-            endpoint=f'/{alias}/_settings/index.analysis.filter.compound_capture.patterns',
-            headers={'Accept': 'application/json'},
-        )
-        hook.check_response(response)
-        return set(p["settings"]["index"]["analysis"]["filter"]["compound_capture"]["patterns"][0] for _, p in response.json().items())
+    def extract_policy(settings):
+        policies = set(p["settings"]["index"]["lifecycle"]["name"] for _, p in settings.items())
+        assert all(p in POLICY_MAPPING for p in policies), f"Invalid policies: {policies}"
+        assert len(set(POLICY_MAPPING.get(p) for p in policies)) == 1, f"Retention period is not unique: {policies}"
+        return next(iter(set(POLICY_MAPPING.get(p) for p in policies)))
 
     @task
-    def verify_mapping(mappings):
+    def extract_mapping(settings):
+        mappings = set(p["settings"]["index"]["analysis"]["filter"]["compound_capture"]["patterns"][0] for _, p in
+            settings.items())
         assert all(m == "(!?[^@!@]+)@!@" for m in mappings), f"Invalid mappings: {mappings}"
         return next(iter(mappings))
 
-    @task()
+    @task
     def group_aliases_by_base(aliases: list) -> list:
         grouped = defaultdict(list)
         for alias_entry in aliases:
             alias = alias_entry["alias"]
-            if not any(r.fullmatch(alias) for r in regex_mapping.values()):
+            if not any(r.fullmatch(alias) for r in REGEX_MAPPING.values()):
                 continue
-            grouped[base_regex.match(alias).group(0)].append(alias_entry)
+            grouped[BASE_REGEX.match(alias).group(0)].append(alias_entry)
         return [{"base_alias": k, "aliases": v} for k, v in grouped.items()]
 
-
     @task
-    def retention_from_policies(policies):
-        assert all(p in policy_mapping for p in policies), f"Invalid policies: {policies}"
-        assert len(set(policy_mapping.get(p) for p in policies)) == 1, f"Retention period is not unique: {policies}"
-        return next(iter(set(policy_mapping.get(p) for p in policies)))
-
-    @task
-    def verify_write_aliases(alias, alias_list, num_months):
-        regex = regex_mapping["write"]
+    def missing_write_aliases(alias, alias_list, num_months):
+        regex = REGEX_MAPPING["write"]
 
         write_aliases= {alias['alias']: alias["index"] for alias in alias_list if
                 regex.fullmatch(alias['alias']) and alias["is_write_index"]}
@@ -104,13 +97,14 @@ def es_poc_dag():
         for monthly_alias in monthly_aliases(alias, start_date, num_months):
             if monthly_alias not in write_aliases:
                 missing_aliases.append(monthly_alias)
-        assert len(missing_aliases) == 0, f"Missing aliases: {missing_aliases}"
+        return missing_aliases
 
     @task_group
     def verify_alias(base_alias, aliases):
-        mapping = verify_mapping(mappings=fetch_mappings(alias=base_alias))
-        retention = retention_from_policies(policies=fetch_policies(alias=base_alias))
-        verify_write_aliases(alias=base_alias, alias_list=aliases, num_months=retention)
+        settings = fetch_alias_settings(alias=base_alias)
+        mapping = extract_mapping(settings=settings)
+        retention = extract_policy(settings=settings)
+        missing_write_aliases(alias=base_alias, alias_list=aliases, num_months=retention)
 
 
     verify_alias.partial().expand_kwargs(group_aliases_by_base(fetch_aliases()))
