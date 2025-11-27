2b11eeb9a455284e5ca071a0b1debc8b97e1e386
Emmanuel <echang@medallia.com>
2025-11-27 14:36:52 -0300

    testing

---
+++ fnv.py
@@ -0,0 +1,74 @@
+import datetime
+import re
+from collections import defaultdict
+from pprint import pprint
+from typing import Iterator
+
+from airflow.decorators import task, dag, task_group
+from airflow.providers.http.hooks.http import HttpHook
+from dateutil.relativedelta import relativedelta
+
+BASE_PATTERN = r"(\w+)_topic-builder(-\w+)+(\.\w{2,4}){0,2}(\.\w+)(\.\w{2,4}){1,2}-\1"
+BASE_REGEX = re.compile(BASE_PATTERN)
+
+REGEX_MAPPING = {
+    "read": re.compile(rf"^{BASE_PATTERN}$"),
+    "write": re.compile(rf"^{BASE_PATTERN}" + r"-[0-9]{4}-[0-9]{2}-[0-9]{2}$"),
+    "rollover": re.compile(rf"^{BASE_PATTERN}-rollover$"),
+}
+
+POLICY_MAPPING = {
+	"M6": 6,
+	"M6_rollover": 6,
+	"M18": 18,
+	"M18_rollover": 18,
+	"M36": 36,
+	"M36_rollover": 36,
+}
+
+def monthly_aliases(alias: str, start_date: datetime.date, num_months: int) -> Iterator[str]:
+    current_date = start_date
+    for _ in range(num_months):
+        yield f"{alias}-{current_date:%Y-%m-%d}"
+        current_date -= relativedelta(months=1)
+
+@dag(
+    dag_display_name="FNV",
+    tags=["spf", "test", "poc"],
+    description="This is a POC",
+    catchup=False,
+)
+def fnv():
+    hook_get = HttpHook(method='GET', http_conn_id='es-wordtags')
+
+    @task
+    def fetch_aliases():
+        response = hook_get.run(
+            endpoint='/_cat/aliases?h=alias,index,is_write_index',
+            headers={'Accept': 'application/json'},
+        )
+        hook_get.check_response(response)
+        return [r for r in response.json() if BASE_REGEX.match(r["alias"])]
+
+    @task
+    def fetch_indices():
+        response = hook_get.run(
+            endpoint='/_cat/indices?h=index&format=json',
+            headers={'Accept': 'application/json'},
+        )
+        hook_get.check_response(response)
+        return [r["index"] for r in response.json() if BASE_REGEX.search(r["index"])]
+
+
+    @task
+    def identify_indices_without_read_alias(all_indices, all_aliases):
+        indices_without_read_alias = []
+        indices_with_read_alias = [r["index"] for r in all_aliases if REGEX_MAPPING["read"].match(r["alias"])]
+        for index in all_indices:
+            if index not in indices_with_read_alias:
+                indices_without_read_alias.append(index)
+        return indices_without_read_alias
+
+    identify_indices_without_read_alias(fetch_indices(), fetch_aliases())
+
+fnv()
