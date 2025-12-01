ec57ea25d0d747bdb0d77142682efad8e0d2dd72
Emmanuel <echang@medallia.com>
2025-12-01 19:33:30 -0300

    testing

--- fnv.py
+++ fnv.py
@@ -183,7 +183,7 @@ def fnv():
 
     @task.branch
     def choose_branch(missing_aliases):
-        if len(list(missing_aliases.value)) > 0:
+        if missing_aliases.success and len(list(missing_aliases.value)) > 0:
             return 'aaaaaaaa.task_a'
         return 'aaaaaaaa.task_b'
 
