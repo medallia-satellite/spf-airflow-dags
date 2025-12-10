d05e51d4c5b6dcddc58b54f6acd1d068ab2ebb92
Emmanuel <echang@medallia.com>
2025-12-10 14:44:32 -0300

    troubleshoot

--- reconcile_aliases_dag.py
+++ reconcile_aliases_dag.py
@@ -27,7 +27,7 @@ def reconcile_aliases_dag():
             if not any(r.fullmatch(alias) for r in ALIAS_REGEX_MAPPING.values()):
                 continue
             result[index].append(alias)
-        return [success(key=k, value=v) for k, v in result.items()]
+        return [success(key=k, value=v) for k, v in result.items() if len(v) < 3]
 
     @task
     def aaaaaaa(data: Result):
