e45474917e4a17a29444ea6b68554200648e5604
Emmanuel <echang@medallia.com>
2025-12-12 18:07:56 -0300

    wip

--- wip_dag.py
+++ wip_dag.py
@@ -120,6 +120,7 @@ def wip_dag():
                             },
                            'number_of_replicas': '1',
                            'number_of_shards': 1}}}}
+            _ = index_template.pop("composed_of")
             if index_template != ref:
                 return failure(key=tenant, error=f"Invalid template: {index_template}")
             return success(key=tenant, value="")
