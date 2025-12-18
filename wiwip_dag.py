2dba2281bfccde9f0099593eadbf5ae4c889fc7c
Emmanuel <echang@medallia.com>
2025-12-18 16:41:57 -0300

    wiwip

--- wiwip_dag.py
+++ wiwip_dag.py
@@ -1,12 +1,59 @@
 from collections import defaultdict
+from typing import TypedDict, Optional, Any, List
 
-from airflow.decorators import dag, task_group
+from airflow.decorators import dag, task_group, task
+from airflow.operators.python import get_current_context
 from airflow.providers.http.hooks.http import HttpHook
 
-from repo.elasticsearch_client import *
 from repo.fix_and_verify import *
-from repo.utils import *
 
+
+def xcom_pull(task_id: str, key: str) -> Any:
+    context = get_current_context()
+    ti = context["ti"]
+    print(f"xcom_pull {task_id} {key}")
+    return ti.xcom_pull(task_ids=task_id, key=key)
+
+def xcom_push(key: str, value: Any) -> None:
+    context = get_current_context()
+    ti = context["ti"]
+    print(f"xcom_push {key} {value}")
+    ti.xcom_push(key, value)
+
+def generate_past_month_starts(n):
+    current_month_start = datetime.datetime.today().replace(day=1, hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc) + relativedelta(months=1)
+    return [current_month_start - relativedelta(months=i) for i in range(n)]
+
+def fetch_from_endpoint(hook: HttpHook, endpoint: str):
+    response = hook.run(
+        endpoint=endpoint,
+        headers={'Accept': 'application/json'},
+    )
+    hook.check_response(response)
+    return response.json()
+
+class Context(TypedDict, total=False):
+    tenant: str
+    success: bool
+    error: Optional[str]
+    value: Optional[Any]
+    retention: Optional[int]
+
+def success(context: Context, value: Any = None) -> Context:
+    return Context(
+        tenant=context["tenant"],
+        success=True,
+        value=value if value else context["value"],
+        retention=context["retention"],
+    )
+
+def failure(context: Context, error: Any = None) -> Context:
+    return Context(
+        tenant=context["tenant"],
+        success=False,
+        error=error if error else context["value"],
+        retention=context["retention"],
+    )
 
 @dag(
     dag_display_name="WIP2",
@@ -16,49 +63,43 @@ from repo.utils import *
     catchup=False,
 )
 def wiwip_dag():
-    @task
-    def extract_success(data: List[Result]) -> List[Result]:
-        return [d for d in data if d["success"]]
-
-    def generate_past_month_starts(n):
-        current_month_start = datetime.datetime.today().replace(day=1, hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc) + relativedelta(months=1)
-        return [current_month_start - relativedelta(months=i) for i in range(n)]
-
-    def _filter_and_push(results, filter_fn = lambda _: True) -> List[Result]:
-        filtered = []
+    def _filter_and_push(results, filter_fn) -> None:
         for k, v in results.items():
             if filter_fn(k):
                 xcom_push(k, v)
-                filtered.append(success(key=k, value=None))
-        return filtered
 
     @task_group
-    def fetch_and_group_indices(hook: HttpHook):
+    def fetch_and_group_indices(hook: HttpHook) -> List[Context]:
         @task
         def fetch(h: HttpHook) -> List[str]:
             results = fetch_from_endpoint(h, "/_cat/indices?h=index&format=json")
             return [r["index"] for r in results if INDEX_REGEX.match(r["index"])]
 
         @task
-        def group_by_tenant(data: list):
-            result = defaultdict(list)
+        def group_by_tenant(data: list) -> List[Context]:
+            results = defaultdict(list)
             for index in data:
-                result[BASE_REGEX.search(index).group(0)].append(index)
-            return _filter_and_push(result)
+                results[BASE_REGEX.search(index).group(0)].append(index)
+
+            grouped = []
+            for k, v in results.items():
+                xcom_push(k, v)
+                grouped.append(Context(tenant=k, success=True))
+            return grouped
 
         return group_by_tenant(fetch(hook))
 
     @task_group
-    def ilm_settings(hook: HttpHook, upstream: List[Result]) -> List[Result]:
+    def ilm_settings(hook: HttpHook, upstream: List[Context]) -> List[Context]:
         @task
-        def fetch(h: HttpHook):
+        def fetch(h: HttpHook) -> bool:
             results = fetch_from_endpoint(h, "/_settings/index.lifecycle.name,index.lifecycle.rollover_alias")
-            return _filter_and_push(results, lambda x: INDEX_REGEX.match(x))
+            _filter_and_push(results, lambda x: INDEX_REGEX.match(x))
+            return True
 
         @task
-        def verify(data: Result) -> Result:
-            tenant = data["key"]
-            indices = xcom_pull("fetch_and_group_indices.group_by_tenant", tenant)
+        def verify(c: Context) -> Context:
+            indices = xcom_pull("fetch_and_group_indices.group_by_tenant", c["tenant"])
             il_list = []
             for index in indices:
                 if s := xcom_pull("ilm_settings.fetch", index):
@@ -67,120 +108,107 @@ def wiwip_dag():
                     print(f"No settings for {index}")
 
             if any("name" not in il for il in il_list):
-                return failure(key=tenant, error=f"Invalid policies: {il_list}")
+                return failure(context=c, error=f"Invalid policies: {il_list}")
 
             policies = set(il["name"] for il in il_list)
             if not all(p in POLICY_MAPPING for p in policies):
-                return failure(key=tenant, error=f"Invalid policies: {policies}")
+                return failure(context=c, error=f"Invalid policies: {policies}")
 
             if len(set(POLICY_MAPPING.get(p) for p in policies)) != 1:
-                return failure(key=tenant, error=f"Retention period is not unique: {policies}")
+                return failure(context=c, error=f"Retention period is not unique: {policies}")
 
             if any("rollover_alias" not in il for il in il_list):
-                return failure(key=tenant,
+                return failure(context=c,
                                error=f"No rollover alias {[il for il in il_list if 'rollover_alias' not in il]}")
 
             rollover = set(il["rollover_alias"] for il in il_list)
-            if not all(ALIAS_REGEX_MAPPING["rollover"].match(a) for a in rollover):
-                return failure(key=tenant, error=f"Invalid rollover alias {rollover}")
+            if not all(a == f"{c['tenant']}-rollover" for a in rollover):
+                return failure(context=c, error=f"Invalid rollover alias {rollover}")
 
-            if len(rollover) != 1:
-                return failure(key=tenant, error=f"Rollover alias is not unique {rollover}")
+            retention = next(iter(set(POLICY_MAPPING.get(p) for p in policies)))
 
-            xcom_push(key=tenant, value={
-                "retention": next(iter(set(POLICY_MAPPING.get(p) for p in policies))),
-                "rollover_alias": next(iter(rollover)),
-            })
-
-            return success(key=tenant, value=None)
+            return Context(tenant=c["tenant"], success=True, retention=retention)
 
         f = fetch(hook)
-        e = extract_success(data=upstream)
-        v = verify.expand(data=e)
+        v = verify.expand(data=upstream)
         f >> v
         return v
 
     @task_group
-    def index_templates(hook: HttpHook, upstream: List[Result]) -> List[Result]:
+    def index_templates(hook: HttpHook, upstream: List[Context]) -> List[Context]:
         @task
-        def fetch(h: HttpHook) -> List[Result]:
+        def fetch(h: HttpHook) -> bool:
             results = fetch_from_endpoint(h, "/_index_template/*-rollover")
-            return _filter_and_push(
+            _filter_and_push(
                 {r["name"]: r["index_template"] for r in results["index_templates"]},
                 lambda x: ALIAS_REGEX_MAPPING["rollover"].match(x),
             )
+            return True
 
         @task
-        def verify(data: Result) -> Result:
-            tenant = data["key"]
-            settings = xcom_pull("ilm_settings.verify", tenant)[0]
-            index_template = xcom_pull("index_templates.fetch", settings["rollover_alias"])
+        def verify(c: Context) -> Context:
+            index_template = xcom_pull("index_templates.fetch", f'{c["tenant"]}-rollover')
             _ = index_template.pop("composed_of")
 
-            if index_template != expected_index_template(tenant=tenant, retention_months=settings["retention"]):
-                return failure(key=tenant, error=f"Invalid index template: {index_template}")
+            if index_template != expected_index_template(tenant=c["tenant"], retention_months=c["retention"]):
+                return failure(context=c, error=f"Invalid index template: {index_template}")
 
-            xcom_push(key=tenant, value=index_template)
-            return success(key=tenant, value=index_template)
+            return success(context=c)
 
         f = fetch(hook)
-        e = extract_success(data=upstream)
-        v = verify.expand(data=e)
+        v = verify.expand(data=upstream)
         f >> v
         return v
 
     @task_group
-    def monthly_indices(hook: HttpHook, upstream: List[Result]) -> List[Result]:
+    def monthly_indices(hook: HttpHook, upstream: List[Context]) -> List[Context]:
         @task
-        def verify(data: Result) -> Result:
-            tenant = data["key"]
-            indices = xcom_pull("fetch_and_group_indices.group_by_tenant", tenant)
-            num_months = xcom_pull("ilm_settings.verify", tenant)[0]["retention"]
+        def verify(c: Context) -> Context:
+            indices = xcom_pull("fetch_and_group_indices.group_by_tenant", c["tenant"])
 
-            for month_start in generate_past_month_starts(num_months):
-                if not [index for index in indices if f"{tenant}-{month_start:%Y-%m-%d}" in index]:
-                    return failure(key=tenant, error="Missing index")
-            return success(key=tenant, value=None)
-        e = extract_success(data=upstream)
-        v = verify.expand(data=e)
+            for month_start in generate_past_month_starts(c["retention"]):
+                if not [index for index in indices if f'{c["tenant"]}-{month_start:%Y-%m-%d}' in index]:
+                    return failure(context=c, error="Missing index")
+            return success(context=c)
+        v = verify.expand(data=upstream)
         return v
 
     @task_group
-    def aliases(hook: HttpHook, upstream: List[Result]) -> List[Result]:
+    def aliases(hook: HttpHook, upstream: List[Context]) -> List[Context]:
         @task
-        def fetch(h: HttpHook) -> List[Result]:
+        def fetch(h: HttpHook) -> bool:
             results = fetch_from_endpoint(h, "/_aliases")
-            return _filter_and_push(results, lambda x: INDEX_REGEX.match(x))
+            _filter_and_push(results, lambda x: INDEX_REGEX.match(x))
+            return True
+
         @task
-        def verify(data: Result) -> Result:
-            tenant = data["key"]
-            indices = xcom_pull("refetch_and_group_indices.group_by_tenant", tenant)
-            num_months = xcom_pull("ilm_settings.verify", tenant)["retention"]
+        def verify(c: Context) -> Context:
+            indices = xcom_pull("refetch_and_group_indices.group_by_tenant", c["tenant"])
 
             active_indices = []
-            for month_start in generate_past_month_starts(num_months):
-                active_indices += [index for index in indices if f"{tenant}-{month_start:%Y-%m-%d}" in index]
+            for month_start in generate_past_month_starts(c["retention"]):
+                active_indices += [index for index in indices if f'{c["tenant"]}-{month_start:%Y-%m-%d}' in index]
 
             for index in active_indices:
                 aa = xcom_pull("aliases.fetch", index)
                 if not all([any(r.fullmatch(alias) for r in ALIAS_REGEX_MAPPING.values()) for alias in
                             aa["aliases"].keys()]):
-                    return failure(key=tenant, error="Alias not complete")
-
-            return success(key=tenant, value=None)
+                    return failure(context=c, error="Alias not complete")
+            return success(context=c)
         f = fetch(hook)
-        e = extract_success(data=upstream)
-        v = verify.expand(data=e)
+        v = verify.expand(data=upstream)
         f >> v
         return v
+
     hook_get = HttpHook(method='GET', http_conn_id='es-wordtags')
     t1 = fetch_and_group_indices(hook=hook_get)
     t2 = ilm_settings(hook=hook_get, upstream=t1)
     t3 = index_templates(hook=hook_get, upstream=t2)
     t4 = monthly_indices(hook=hook_get, upstream=t3)
+    # fix errors and refetch
     t5 = fetch_and_group_indices.override(group_id="refetch_and_group_indices")(hook=hook_get)
     t4 >> t5
-    t6 = monthly_indices(hook=hook_get, upstream=t4)
+    t6 = monthly_indices(hook=hook_get, upstream=t5)
     t5 >> t6
 
 
