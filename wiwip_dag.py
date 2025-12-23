beb607990f84964fd558400dfcb0f064a10540c8
Emmanuel <echang@medallia.com>
2025-12-23 14:36:47 -0300

    wiwip

--- wiwip_dag.py
+++ wiwip_dag.py
@@ -3,20 +3,23 @@ import json
 import pprint
 from collections import defaultdict
 from typing import TypedDict, Optional, Any, List
+import datetime
+from dateutil.relativedelta import relativedelta
+
+import re
 
 from airflow.decorators import dag, task_group, task
 from airflow.models import Param
 from airflow.operators.python import get_current_context
 from airflow.providers.http.hooks.http import HttpHook
 
-import datetime
-import re
-
-from dateutil.relativedelta import relativedelta
 
 BASE_PATTERN = r"(\w+)_topic-builder(-\w+)+(\.\w{2,4}){0,2}(\.\w+)(\.\w{2,4}){1,2}-\1"
 BASE_REGEX = re.compile(BASE_PATTERN)
-INDEX_PATTERN = rf"^seaas-{BASE_PATTERN}" + r"-(?P<month>[0-9]{4}-[0-9]{2}-[0-9]{2})-(?P<tenant_id>[0-9]+)-(?P<suffix>[0-9]+)$"
+INDEX_PATTERN = (
+    rf"^seaas-{BASE_PATTERN}"
+    + r"-(?P<month>[0-9]{4}-[0-9]{2}-[0-9]{2})-(?P<tenant_id>[0-9]+)-(?P<suffix>[0-9]+)$"
+)
 INDEX_REGEX = re.compile(INDEX_PATTERN)
 
 ALIAS_REGEX_MAPPING = {
@@ -25,13 +28,14 @@ ALIAS_REGEX_MAPPING = {
     "rollover": re.compile(rf"{BASE_PATTERN}-rollover"),
 }
 POLICY_MAPPING = {
-	"M6": 6,
-	"M6_rollover": 6,
-	"M18": 18,
-	"M18_rollover": 18,
-	"M36": 36,
-	"M36_rollover": 36,
+    "M6": 6,
+    "M6_rollover": 6,
+    "M18": 18,
+    "M18_rollover": 18,
+    "M36": 36,
+    "M36_rollover": 36,
 }
+
 
 def extract_index_details(index):
     tenant = BASE_REGEX.search(index).group(0)
@@ -39,7 +43,9 @@ def extract_index_details(index):
     tenant_id = m["tenant_id"]
     suffix = m["suffix"]
     month = m["month"]
-    should_rollover = datetime.date.fromisoformat(month) == datetime.date.today().replace(day=1) + relativedelta(months=1)
+    should_rollover = datetime.date.fromisoformat(
+        month
+    ) == datetime.date.today().replace(day=1) + relativedelta(months=1)
     read_alias = ALIAS_REGEX_MAPPING["read"].search(index).group(0)
     write_alias = ALIAS_REGEX_MAPPING["write"].search(index).group(0)
     rollover_alias = f"{read_alias}-rollover"
@@ -54,12 +60,14 @@ def extract_index_details(index):
         "should_rollover": should_rollover,
     }
 
+
 def chain_on_success(func):
     @functools.wraps(func)
     def wrapper(context):
         if not context["success"]:
             return context
         return func(context)
+
     return wrapper
 
 
@@ -69,47 +77,52 @@ def xcom_pull(task_id: str, key: str) -> Any:
     print(f"xcom_pull {task_id} {key}")
     return ti.xcom_pull(task_ids=task_id, key=key)
 
+
 def xcom_push(key: str, value: Any) -> None:
     context = get_current_context()
     ti = context["ti"]
     print(f"xcom_push {key} {value}")
     ti.xcom_push(key, value)
 
+
 def generate_past_month_starts(n):
-    current_month_start = (datetime.datetime.today()
-                           .replace(day=1, hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc)
-                           + relativedelta(months=1))
+    current_month_start = datetime.datetime.today().replace(
+        day=1, hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc
+    ) + relativedelta(months=1)
     return [current_month_start - relativedelta(months=i) for i in range(n)]
 
 
 def http_hook_put(conn_id: str, endpoint: str, data: str):
-    hook_put = HttpHook(method='PUT', http_conn_id=conn_id)
+    hook_put = HttpHook(method="PUT", http_conn_id=conn_id)
     response = hook_put.run(
-        endpoint=f'/{endpoint}?pretty',
-        headers={'Content-Type': 'application/json'},
-        data=data
+        endpoint=f"/{endpoint}?pretty",
+        headers={"Content-Type": "application/json"},
+        data=data,
     )
     hook_put.check_response(response)
     return response.json()
 
+
 def http_hook_post(conn_id: str, endpoint: str, data: str):
-    hook_post = HttpHook(method='POST', http_conn_id=conn_id)
+    hook_post = HttpHook(method="POST", http_conn_id=conn_id)
     response = hook_post.run(
-        endpoint=f'/{endpoint}?pretty',
-        headers={'Content-Type': 'application/json'},
-        data=data
+        endpoint=f"/{endpoint}?pretty",
+        headers={"Content-Type": "application/json"},
+        data=data,
     )
     hook_post.check_response(response)
     return response.json()
 
+
 def http_hook_get(conn_id: str, endpoint: str):
-    hook_get = HttpHook(method='GET', http_conn_id=conn_id)
+    hook_get = HttpHook(method="GET", http_conn_id=conn_id)
     response = hook_get.run(
         endpoint=endpoint,
-        headers={'Accept': 'application/json'},
+        headers={"Accept": "application/json"},
     )
     hook_get.check_response(response)
     return response.json()
+
 
 class Context(TypedDict, total=False):
     tenant: str
@@ -120,6 +133,7 @@ class Context(TypedDict, total=False):
     value: Optional[Any]
     retention: Optional[int]
 
+
 def success(context: Context, stage: str, value: Any = None) -> Context:
     return Context(
         tenant=context["tenant"],
@@ -129,6 +143,7 @@ def success(context: Context, stage: str, value: Any = None) -> Context:
         value=value if value else context.get("value"),
         retention=context.get("retention"),
     )
+
 
 def failure(context: Context, stage: str, error: Any = None) -> Context:
     return Context(
@@ -148,7 +163,6 @@ def failure(context: Context, stage: str, error: Any = None) -> Context:
     max_active_runs=1,
     catchup=False,
     params={"db_conn": Param("es-testing", type="string")},
-
 )
 def wiwip_dag():
     dry_run = True
@@ -161,11 +175,13 @@ def wiwip_dag():
     @task
     def report(upstream: List[Context], stage: str) -> None:
         errors = [x for x in upstream if not x["success"]]
-        print(f"""
+        print(
+            f"""
         success: {len(upstream) - len(errors)}/{len(upstream)}
         errors: {len(errors)}/{len(upstream)}
         errors in stage {stage}: {len([e for e in errors if e["stage"] == stage])}/{len(errors)}
-        """)
+        """
+        )
         for i, c in enumerate(upstream):
             if not c["success"] and c["stage"] == stage:
                 print(f"{i}: {c['tenant']}")
@@ -178,21 +194,29 @@ def wiwip_dag():
         results = defaultdict(list)
         for index in [r["index"] for r in fetched if INDEX_REGEX.match(r["index"])]:
             results[
-                (BASE_REGEX.search(index).group(0), INDEX_REGEX.fullmatch(index).groupdict()["tenant_id"])].append(
-                index)
+                (
+                    BASE_REGEX.search(index).group(0),
+                    INDEX_REGEX.fullmatch(index).groupdict()["tenant_id"],
+                )
+            ].append(index)
 
         grouped = []
         for k, v in results.items():
             xcom_push(k[0], v)
-            grouped.append(Context(tenant=k[0], tenant_id=k[1], stage=stage, success=True))
+            grouped.append(
+                Context(tenant=k[0], tenant_id=k[1], stage=stage, success=True)
+            )
         return grouped
 
     @task_group
     def ilm_settings(conn_id: str, upstream: List[Context]) -> List[Context]:
         tg_stage = "ilm_settings"
+
         @task
         def fetch(h: str) -> bool:
-            results = http_hook_get(h, "/_settings/index.lifecycle.name,index.lifecycle.rollover_alias")
+            results = http_hook_get(
+                h, "/_settings/index.lifecycle.name,index.lifecycle.rollover_alias"
+            )
             _filter_and_push(results, lambda x: INDEX_REGEX.match(x))
             return True
 
@@ -208,17 +232,35 @@ def wiwip_dag():
                     print(f"No settings for {index}")
 
             if len(indices) != len(il_list):
-                return failure(context=context, stage=tg_stage, error="Some indices are missing ILM settings")
+                return failure(
+                    context=context,
+                    stage=tg_stage,
+                    error="Some indices are missing ILM settings",
+                )
 
             retention = list(set(POLICY_MAPPING.get(il.get("name")) for il in il_list))
             if len(retention) != 1 or retention[0] not in POLICY_MAPPING.values():
-                return failure(context=context, stage=tg_stage, error=f'Invalid policies: {set(il.get("name") for il in il_list)}')
+                return failure(
+                    context=context,
+                    stage=tg_stage,
+                    error=f'Invalid policies: {set(il.get("name") for il in il_list)}',
+                )
 
             rollover = set(il.get("rollover_alias") for il in il_list)
             if not all(r == f"{context['tenant']}-rollover" for r in rollover):
-                return failure(context=context, stage=tg_stage, error=f"Invalid rollover alias {rollover}")
+                return failure(
+                    context=context,
+                    stage=tg_stage,
+                    error=f"Invalid rollover alias {rollover}",
+                )
 
-            return Context(tenant=context["tenant"], tenant_id=context["tenant_id"], stage=tg_stage, success=True, retention=retention[0])
+            return Context(
+                tenant=context["tenant"],
+                tenant_id=context["tenant_id"],
+                stage=tg_stage,
+                success=True,
+                retention=retention[0],
+            )
 
         verified = verify.expand(context=upstream)
         fetch(conn_id) >> verified
@@ -228,6 +270,7 @@ def wiwip_dag():
     @task_group
     def index_templates(conn_id: str, upstream: List[Context]) -> List[Context]:
         tg_stage = "index_templates"
+
         @task
         def fetch(h: str) -> bool:
             results = http_hook_get(h, "/_index_template/*-rollover")
@@ -240,11 +283,19 @@ def wiwip_dag():
         @task
         @chain_on_success
         def verify(context: Context) -> Context:
-            index_template = xcom_pull("index_templates.fetch", f'{context["tenant"]}-rollover')
+            index_template = xcom_pull(
+                "index_templates.fetch", f'{context["tenant"]}-rollover'
+            )
             _ = index_template.pop("composed_of")
 
-            if index_template != expected_index_template(tenant=context["tenant"], retention_months=context["retention"]):
-                return failure(context=context, stage=tg_stage, error=f"Invalid index template: {index_template}")
+            if index_template != expected_index_template(
+                tenant=context["tenant"], retention_months=context["retention"]
+            ):
+                return failure(
+                    context=context,
+                    stage=tg_stage,
+                    error=f"Invalid index template: {index_template}",
+                )
 
             return success(context=context, stage=tg_stage)
 
@@ -263,7 +314,10 @@ def wiwip_dag():
             indices = xcom_pull("fetch_indices_per_tenant", context["tenant"])
             missing = []
             for month_start in generate_past_month_starts(context["retention"]):
-                if not any(f'{context["tenant"]}-{month_start:%Y-%m-%d}' in index for index in indices):
+                if not any(
+                    f'{context["tenant"]}-{month_start:%Y-%m-%d}' in index
+                    for index in indices
+                ):
                     missing.append(month_start)
             if missing:
                 return failure(context=context, stage=tg_stage, error=missing)
@@ -284,7 +338,7 @@ def wiwip_dag():
                         details["read_alias"]: {"is_write_index": False},
                         details["write_alias"]: {"is_write_index": True},
                         details["rollover_alias"]: {"is_write_index": False},
-                    }
+                    },
                 }
                 if dry_run:
                     print(f"Dry run: {index} - {payload}")
@@ -301,6 +355,7 @@ def wiwip_dag():
     @task_group
     def aliases(conn_id: str, upstream: List[Context]) -> List[Context]:
         tg_stage = "aliases"
+
         @task
         def fetch(h: str, data: List[Context]) -> List[Context]:
             results = http_hook_get(h, "/_aliases")
@@ -314,13 +369,23 @@ def wiwip_dag():
 
             active_indices = []
             for month_start in generate_past_month_starts(context["retention"]):
-                active_indices += [index for index in indices if f'{context["tenant"]}-{month_start:%Y-%m-%d}' in index]
+                active_indices += [
+                    index
+                    for index in indices
+                    if f'{context["tenant"]}-{month_start:%Y-%m-%d}' in index
+                ]
 
             missing = []
             for index in active_indices:
-                index_aliases = xcom_pull("aliases.fetch", index).get("aliases", dict()).keys()
-                if not all([any(r.fullmatch(alias) for alias in index_aliases) for r in
-                            ALIAS_REGEX_MAPPING.values()]):
+                index_aliases = (
+                    xcom_pull("aliases.fetch", index).get("aliases", dict()).keys()
+                )
+                if not all(
+                    [
+                        any(r.fullmatch(alias) for alias in index_aliases)
+                        for r in ALIAS_REGEX_MAPPING.values()
+                    ]
+                ):
                     missing.append(index)
 
             if missing:
@@ -335,9 +400,27 @@ def wiwip_dag():
             for index in context["error"]:
                 details = extract_index_details(index)
                 actions += [
-                    {"add": {"index": index, "alias": details["read_alias"], "is_write_index": False}},
-                    {"add": {"index": index, "alias": details["write_alias"], "is_write_index": True}},
-                    {"add": {"index": index, "alias": details["rollover_alias"], "is_write_index": details["should_rollover"]}},
+                    {
+                        "add": {
+                            "index": index,
+                            "alias": details["read_alias"],
+                            "is_write_index": False,
+                        }
+                    },
+                    {
+                        "add": {
+                            "index": index,
+                            "alias": details["write_alias"],
+                            "is_write_index": True,
+                        }
+                    },
+                    {
+                        "add": {
+                            "index": index,
+                            "alias": details["rollover_alias"],
+                            "is_write_index": details["should_rollover"],
+                        }
+                    },
                 ]
             print(f"{context['tenant']}: {actions}")
 
@@ -357,6 +440,7 @@ def wiwip_dag():
     @task_group
     def rollover_alias(conn_id: str, upstream: List[Context]) -> List[Context]:
         tg_stage = "rollover_alias"
+
         @task
         def fetch_indices_in_alias(h: str, context: Context) -> Context:
             if not context["success"]:
@@ -390,11 +474,21 @@ def wiwip_dag():
             indices = xcom_pull("rollover_alias.fetch_indices_in_alias", alias)[0]
             actions = []
             if index := context["error"]:
-                actions.append({"add": {"index": index, "alias": alias,"is_write_index": False}})
+                actions.append(
+                    {"add": {"index": index, "alias": alias, "is_write_index": False}}
+                )
             for index, is_write_alias in indices:
                 details = extract_index_details(index)
                 if details["should_rollover"]:
-                    actions.append({"add": {"index": index, "alias": alias, "is_write_index": True}})
+                    actions.append(
+                        {
+                            "add": {
+                                "index": index,
+                                "alias": alias,
+                                "is_write_index": True,
+                            }
+                        }
+                    )
                     break
 
             if dry_run:
@@ -404,8 +498,9 @@ def wiwip_dag():
             http_hook_post(c, f"/_aliases/{alias}", json.dumps({"actions": actions}))
             return success(context=context, stage=tg_stage)
 
-
-        verified = verify.expand(context=fetch_indices_in_alias.partial(h=conn_id).expand(context=upstream))
+        verified = verify.expand(
+            context=fetch_indices_in_alias.partial(h=conn_id).expand(context=upstream)
+        )
         report(upstream=verified, stage=tg_stage)
         return fix.partial(c=conn_id).expand(context=verified)
 
