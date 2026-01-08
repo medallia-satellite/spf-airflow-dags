a01cbdc57848b8b07155e69c1942126633586185
Emmanuel <echang@medallia.com>
2026-01-08 14:56:12 -0300

    including repo in path

--- fix_monthly_indices_dag.py
+++ fix_monthly_indices_dag.py
@@ -1,8 +1,11 @@
 import json
+import os
 from typing import List
 
 from airflow.decorators import dag, task
 from airflow.models import Param
+import sys
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
 from fix_and_verify import (
     INDEX_REGEX,
