cbb47ca83b565f7faa9cd7c1a5377e261d7a72cd
Emmanuel <echang@medallia.com>
2026-03-12 15:06:41 -0300

    troubleshoot

--- fix_and_verify_dag.py
+++ fix_and_verify_dag.py
@@ -134,8 +134,8 @@ def verify_dag():
             return success(context=context, stage=tg_stage)
 
         verified = verify.expand(context=fetch(upstream))
-        select_eligible_for_fix(upstream=verified, stage=tg_stage)
-        return verified
+
+        return select_eligible_for_fix(upstream=verified, stage=tg_stage)
 
     @task_group
     def index_templates(upstream: List[Context]) -> List[Context]:
@@ -169,8 +169,8 @@ def verify_dag():
             return success(context=context, stage=tg_stage)
 
         verified = verify.expand(context=fetch(upstream))
-        select_eligible_for_fix(upstream=verified, stage=tg_stage)
-        return verified
+
+        return select_eligible_for_fix(upstream=verified, stage=tg_stage)
 
     @task_group
     def monthly_indices(upstream: List[Context]) -> List[Context]:
