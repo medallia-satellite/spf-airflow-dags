d8a85ea50bb73188464586f486a6dd966ee1ee40
Emmanuel <echang@medallia.com>
2026-05-08 15:55:45 -0300

    testing dag

--- a_cool_name.py
+++ a_cool_name.py
@@ -12,8 +12,8 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
     render_template_as_native_obj=True,
 )
 def a_cool_dag():
-    trigger_child = TriggerDagRunOperator(
-        task_id="trigger_fix_monthly_indices_dag",
+    f1 = TriggerDagRunOperator(
+        task_id="f_wordtags",
         trigger_dag_id="fix_and_verify_dag",  # The DAG ID to trigger
         wait_for_completion=True,  # Wait for the child DAG to finish
         poke_interval=30,
@@ -22,5 +22,44 @@ def a_cool_dag():
             "dry_run": True,
         }
     )
+
+    f2 = TriggerDagRunOperator(
+        task_id="f_wordtags_client_sandbox",
+        trigger_dag_id="fix_and_verify_dag",  # The DAG ID to trigger
+        wait_for_completion=True,  # Wait for the child DAG to finish
+        poke_interval=30,
+        conf={
+            "conn_id": "es-wordtags-client-sandbox",
+            "dry_run": True,
+        }
+    )
+
+
+
+    i1 = TriggerDagRunOperator(
+        task_id="i_wordtags",
+        trigger_dag_id="ilm_keeper_dag",  # The DAG ID to trigger
+        wait_for_completion=True,  # Wait for the child DAG to finish
+        poke_interval=30,
+        conf={
+            "conn_id": "es-wordtags",
+            "dry_run": True,
+        }
+    )
+
+
+    i2 = TriggerDagRunOperator(
+        task_id="i_wordtags_client_sandbox",
+        trigger_dag_id="ilm_keeper_dag",  # The DAG ID to trigger
+        wait_for_completion=True,  # Wait for the child DAG to finish
+        poke_interval=30,
+        conf={
+            "conn_id": "es-wordtags-client-sandbox",
+            "dry_run": True,
+        }
+    )
+
+
+    f1 >> i1 >> f2 >> i2
 
 a_cool_dag()
