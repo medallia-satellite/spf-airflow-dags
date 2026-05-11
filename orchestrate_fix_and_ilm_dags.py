15314457bd21510b75681cb17c72d7235107eacb
Emmanuel <echang@medallia.com>
2026-05-11 15:03:37 -0300

    refactor

---
+++ orchestrate_fix_and_ilm_dags.py
@@ -0,0 +1,37 @@
+from airflow.decorators import dag
+from airflow.models import Variable
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+
+@dag(
+    dag_display_name="A cool DAG name",
+    tags=["spf", "elasticsearch"],
+    description="This nice DAG triggers other dags",
+    max_active_runs=1,
+    schedule=None,
+    catchup=False,
+    render_template_as_native_obj=True,
+)
+def orchestrate_fix_and_ilm():
+    targets = Variable.get(
+        "orchestrate_dag_targets",
+        deserialize_json=True,
+    )
+    for target, config in targets.items():
+        f = TriggerDagRunOperator(
+            task_id=f"fix_{target}",
+            trigger_dag_id="fix_and_verify_dag",
+            wait_for_completion=True,
+            poke_interval=30,
+            conf=config
+        )
+        i = TriggerDagRunOperator(
+            task_id=f"ilm_{target}",
+            trigger_dag_id="ilm_keeper_dag",  # The DAG ID to trigger
+            wait_for_completion=True,  # Wait for the child DAG to finish
+            poke_interval=30,
+            conf=config
+        )
+        f >> i
+
+orchestrate_fix_and_ilm()
