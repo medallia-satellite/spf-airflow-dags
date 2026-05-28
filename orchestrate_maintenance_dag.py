0e4b59e6c881db11bc7b8d02d14f8c1652b1942c
Emmanuel <echang@medallia.com>
2026-05-28 15:50:05 -0300

    fix: add default_var to prevent Airflow Variable lookup errors

--- orchestrate_maintenance_dag.py
+++ orchestrate_maintenance_dag.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from airflow.decorators import dag
 from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 
@@ -19,22 +20,26 @@ def orchestrate_maintenance_dag():
     targets = Variable.get(
         "orchestrate_dag_targets",
         deserialize_json=True,
+        default_var = None,
     )
-    for target, config in targets.items():
-        t1 = TriggerDagRunOperator(
-            task_id=f"reconcile_wordtags_indices__{target}",
-            trigger_dag_id="reconcile_wordtags_indices_dag",
-            wait_for_completion=True,
-            poke_interval=30,
-            conf=config
-        )
-        t2 = TriggerDagRunOperator(
-            task_id=f"finalize_expired_indices__{target}",
-            trigger_dag_id="finalize_expired_indices_dag",
-            wait_for_completion=True,
-            poke_interval=30,
-            conf=config
-        )
-        t1 >> t2
+    if targets:
+        for target, config in targets.items():
+            t1 = TriggerDagRunOperator(
+                task_id=f"reconcile_wordtags_indices__{target}",
+                trigger_dag_id="reconcile_wordtags_indices_dag",
+                wait_for_completion=True,
+                poke_interval=30,
+                conf=config,
+            )
+            t2 = TriggerDagRunOperator(
+                task_id=f"finalize_expired_indices__{target}",
+                trigger_dag_id="finalize_expired_indices_dag",
+                wait_for_completion=True,
+                poke_interval=30,
+                conf=config,
+            )
+            t1 >> t2
+    else:
+        EmptyOperator(task_id="no_targets_configured")
 
 orchestrate_maintenance_dag()
