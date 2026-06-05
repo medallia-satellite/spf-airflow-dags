d0d36ebfef9c7fad08f8cddebf46c4e21ee46a90
Emmanuel <echang@medallia.com>
2026-06-05 16:02:20 -0300

    feat: improve aliases fix

--- reconcile_aliases_dag.py
+++ reconcile_aliases_dag.py
@@ -83,7 +83,7 @@ def reconcile_aliases_dag():
             if not any(is_write_index is True for _, is_write_index in write_alias.get(monthly_alias)):
                 actions.append({
                     "add": {
-                        "index": write_alias.get(monthly_alias)[-1],
+                        "index": write_alias.get(monthly_alias)[-1][0],
                         "alias": monthly_alias,
                         "is_write_index": True,
                     }
