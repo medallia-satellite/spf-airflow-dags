cb3680b23b8630997e28419cdb7f1fb00a181e33
Emmanuel <echang@medallia.com>
2026-06-24 14:13:27 -0300

    refactor

--- es_index_metadata_fix.py
+++ es_index_metadata_fix.py
@@ -19,10 +19,7 @@ from utils import (
     http_hook_get,
     http_hook_put,
     Context,
-    success,
-    failure,
     param_value,
-    chain_on_success,
     http_hook_post,
 )
 
@@ -229,116 +226,10 @@ def es_index_lifecycle_metadata_fix():
             ]
             apply_alias_actions(remove_alias_actions, conn_id, dry_run)
 
-        return Context(success=True, value=expired)
+        return expired
 
-    @task
-    def fetch_tenants() -> List[Context]:
-        fetched = http_hook_get(
-            param_value("conn_id"),
-            "/_cat/aliases",
-            params={"h": "alias", "s": "alias", "format": "json"},
-        )
-
-        return [
-            Context(success=True, tenant=alias)
-            for alias in set(
-                r["alias"]
-                for r in fetched
-                if ALIAS_REGEX_MAPPING["read"].match(r["alias"])
-            )
-        ]
-
-    @task
-    def fetch_retention(context: Context) -> Context:
-        results = http_hook_get(
-            param_value("conn_id"),
-            "/_settings/index.lifecycle.name",
-            params={"flat_settings": "true"},
-        )
-        logging.info(f"Fetched {len(results)} records")
-        oldest_index = min(results)
-        policy_name = results[oldest_index]["settings"]["index.lifecycle.name"]
-        if policy_name not in POLICY_MAPPING:
-            return failure(
-                context=context,
-                stage="fetch_retention",
-                error=f"Invalid retention policy: '{policy_name}'",
-            )
-
-        return Context(
-            success=True,
-            tenant=context["tenant"],
-            retention=POLICY_MAPPING.get(policy_name),
-        )
-
-    @task
-    @chain_on_success
-    def expired_indices(context: Context) -> Context:
-        stage = "expired_indices"
-        conn_id = param_value("conn_id")
-        dry_run = param_value("dry_run")
-
-        results = http_hook_get(
-            conn_id,
-            f"/_cat/indices/{context['tenant']}",
-            params={
-                "s": "index",
-                "h": "index",
-                "format": "json",
-            },
-        )
-        indices = [r["index"] for r in results if INDEX_REGEX.match(r["index"])]
-        if not indices:
-            return failure(
-                context=context,
-                stage=stage,
-                error=f"No indices found for '{context['tenant']}'",
-            )
-
-        expired = [
-            index for index in indices if index_has_expired(index, context["retention"])
-        ]
-
-        add_alias_actions = [
-            {
-                "add": {
-                    "index": index,
-                    "alias": f"temp-expired-{context['tenant']}",
-                    "is_write_index": False,
-                }
-            }
-            for index in expired
-        ]
-
-        apply_alias_actions(add_alias_actions, conn_id, dry_run)
-
-        update_index_settings(
-            f"temp-expired-{context['tenant']}",
-            {"index.lifecycle.indexing_complete": True},
-            conn_id,
-            dry_run,
-        )
-
-        remove_alias_actions = [
-            {"remove": {"index": "*", "alias": f"temp-expired-{context['tenant']}"}}
-        ]
-
-        apply_alias_actions(remove_alias_actions, conn_id, dry_run)
-
-        return success(context=context, stage=stage, value=expired)
-
-    @task
-    def report(contexts: List[Context]):
-        for i, c in enumerate(contexts):
-            if c["value"]:
-                logging.info(f"{i}: {c['tenant']}\n{c['value']}")
-            elif not c["success"]:
-                logging.error(f"{i}: {c['tenant']} - {c['error']}")
 
     reconcile_origination_dates() >> expire_indices()
-    report(
-        expired_indices.expand(context=fetch_retention.expand(context=fetch_tenants()))
-    )
 
 
 es_index_lifecycle_metadata_fix()
