18a4f4c3541158409ca1dc1d33731e54b6cb7c71
Emmanuel <echang@medallia.com>
2026-06-26 10:35:43 -0300

    dry run

--- manual_rollover.py
+++ manual_rollover.py
@@ -191,7 +191,7 @@ def trigger_rollover_dag():
                 conn_id,
                 f"{tenant}-rollover/_rollover/{rollover_target}",
                 json.dumps(payload),
-                params={"dry_run": dry_run},
+                params=json.dumps({"dry_run": dry_run}),
             )
 
             logging.info(f"Response:\n{json.dumps(response, indent=2)}")
